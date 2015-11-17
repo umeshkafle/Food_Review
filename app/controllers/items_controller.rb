e068467be3baaf8752d45400d75a8875a5f991f6
umeshkafle <u.kafle@hotmail.com>
2015-11-17 20:50:41 +0545

    devise gem is added in to the project

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,34 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  private
+  def item_params
+    params.require(:item).permit(:title, :Cuisine, :description, :price)
+  end
+end
